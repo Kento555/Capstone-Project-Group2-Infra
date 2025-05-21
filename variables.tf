4932be6a30eb8df710a10ec7297294fadc7f0b0c
Indigo_cE9 <chrisyeohc@outlook.com>
2025-05-21 21:20:41 +0800

    address tflint error

--- variables.tf
+++ variables.tf
@@ -1,23 +1,29 @@
 variable "env" {
+  type = string
   default = "ce-grp-2-dev"
 }
 
 variable "region" {
+  type = string
   default = "us-east-1"
 }
 
 variable "zone1" {
+  type = string
   default = "us-east-1a"
 }
 
 variable "zone2" {
+  type = string
   default = "us-east-1b"
 }
 
 variable "eks_name" {
+  type = string
   default = "ce-grp-2-eks"
 }
 
 variable "eks_version" {
+  type = string
   default = "1.32"
 }
