1054a36dbffa21895f3188ddbf147b3a585ee3af
normanfung <56857601+normanfung@users.noreply.github.com>
2025-05-30 22:52:14 +0800

    TF: Added irsa_role for service account

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,12 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_iam_openid_connect_provider" "this" {
+  url = replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
+}
+
