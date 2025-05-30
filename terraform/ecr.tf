af4473155394f28b713c862e26a7a6bf0fd5d2f5
normanfung96 <56857601+normanfung@users.noreply.github.com>
2025-05-30 15:59:33 +0800

    TF: Updated ECR name, Added dynamoDB
    * TF: Updated ECR name, Added dynamoDB
    
    * TF: Removed unsused var

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,4 @@
 resource "aws_ecr_repository" "services" {
   for_each = toset(var.microservices)
-  name     = "${var.name_prefix}-${var.ecr_namespace}/${each.value}-${var.env}"
+  name     = "${var.name_prefix}/${var.env}/${each.value}"
 }
