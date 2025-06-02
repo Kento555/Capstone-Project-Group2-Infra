c7a749e5cb79cbd16c07d059020f478e0d165cce
kytan2992 <kytan2992@gmail.com>
2025-06-02 12:22:21 +0800

    INFRA: added S3 buckets and IAM role to allow Loki to store logs

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -133,3 +133,59 @@ resource "aws_iam_role_policy_attachment" "attach_eventbridge_policy" {
   role       = aws_iam_role.irsa_role.name
   policy_arn = aws_iam_policy.eventbridge_put_events.arn
 }
+
+
+# IAM Role for Loki to access S3
+resource "aws_iam_policy" "loki_s3" {
+  name        = "${var.name_prefix}-LokiS3Policy-${var.env}"
+  description = "Policy for Loki to access S3 buckets for chunks and ruler storage"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+        {
+            Sid = "LokiStorage",
+            Effect = "Allow",
+            Action = [
+                "s3:ListBucket",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+            ],
+            Resource = [
+                "${aws_s3_bucket.chunks.arn}",
+                "${aws_s3_bucket.chunks.arn}/*",
+                "${aws_s3_bucket.ruler.arn}",
+                "${aws_s3_bucket.ruler.arn}/*"
+            ]
+        }
+    ]
+})
+}
+
+resource "aws_iam_role" "loki_s3" {
+  name = "${var.name_prefix}-LokiS3-${var.env}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+        {
+            Effect = "Allow",
+            Principal = {
+                Federated = module.eks.oidc_provider_arn
+            },
+            Action = "sts:AssumeRoleWithWebIdentity",
+            Condition = {
+                StringEquals = {
+                    "${module.eks.oidc_provider}:sub": "system:serviceaccount:monitoring:loki",
+                    "${module.eks.oidc_provider}:aud": "sts.amazonaws.com"
+                }
+            }
+        }
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "loki_s3_attach" {
+  role       = aws_iam_role.loki_s3.name
+  policy_arn = aws_iam_policy.loki_s3.arn
+}
---
+++ terraform/s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "chunks" {
+  bucket        = "${var.name_prefix}-loki-chunks-${var.env}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "ruler" {
+  bucket        = "${var.name_prefix}-loki-ruler-${var.env}"
+  force_destroy = true
+}
